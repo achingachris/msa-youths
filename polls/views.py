aa5bf06f78a75690ef537cdf1b63e9f98089fdeb
chris_achinga <achinga.chris@gmail.com>
2023-04-09 20:15:00 +0300

    voting form

--- polls/views.py
+++ polls/views.py
@@ -16,91 +16,44 @@ def index(request):
 
 @login_required
 def vote(request):
-    if request.method == 'POST':
-        user = request.user
-        nominee_id = request.POST.get('nominee')
-        nominee = Nominee.objects.get(pk=nominee_id)
-        timestamp = timezone.now()
-        try:
-            vote = Vote.objects.get(user=user, nominee__category=nominee.category)
-            vote.nominee = nominee
-            vote.timestamp = timestamp
-            vote.save()
-        except ObjectDoesNotExist:
-            vote = Vote(user=user, nominee=nominee, timestamp=timestamp)
-            vote.save()
-        return redirect('vote_success')
-    else:
-        categories = Category.objects.all()
-        context = {'categories': categories}
-        return render(request, 'vote.html', context)
+    # Get all categories
+    categories = Category.objects.all()
 
-# def nominees(request, category_id):
-#     category = Category.objects.get(pk=category_id)
-#     nominees = Nominee.objects.filter(category=category)
-#     context = {'category': category, 'nominees': nominees}
-#     return render(request, 'nominees.html', context)
+    # Check if the user has already voted
+    try:
+        last_vote = request.user.vote_set.latest('timestamp')
+    except Vote.DoesNotExist:
+        last_vote = None
+
+    if last_vote and (timezone.now() - last_vote.timestamp).total_seconds() < 3600:
+        # User has already voted within the last hour, redirect back to wait_for_next_hour
+        return redirect('wait_for_next_hour')
+    
+    # Check if the user has submitted a vote
+    if request.method == 'POST':
+        # Get the selected nominees for each category
+        selected_nominees = {}
+        for category in categories:
+            nominee_id = request.POST.get(str(category.id))
+            if nominee_id:
+                selected_nominees[category.id] = nominee_id
+
+        # Create a vote for each selected nominee
+        for category_id, nominee_id in selected_nominees.items():
+            nominee = get_object_or_404(Nominee, id=nominee_id)
+            vote = Vote(user=request.user, nominee=nominee)
+            vote.save()
+
+        # Redirect back to index
+        return redirect('polls:success')
+
+    # Render the vote template with all categories and their nominees
+    context = {'categories': categories}
+    return render(request, 'vote.html', context)
+
 
 def vote_success(request):
     return render(request, 'vote_success.html')
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-def index(request):
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    context = {"latest_question_list": latest_question_list}
-    return render(request, "polls/index.html", context)
-
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/detail.html", {"question": question})
-
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/results.html", {"question": question})
-
-def vote(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    try:
-        selected_choice = question.choice_set.get(pk=request.POST["choice"])
-    except (KeyError, Choice.DoesNotExist):
-        # Redisplay the question voting form.
-        return render(
-            request,
-            "polls/detail.html",
-            {
-                "question": question,
-                "error_message": "You didn't select a choice.",
-            },
-        )
-    else:
-        selected_choice.votes += 1
-        selected_choice.save()
-        # Always return an HttpResponseRedirect after successfully dealing
-        # with POST data. This prevents data from being posted twice if a
-        # user hits the Back button.
-        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
+def waiting_view(request):
+    return render(request, 'waiting.html')
