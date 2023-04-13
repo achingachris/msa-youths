818368aa9468e62fee140865426525ed69c99e9a
chris_achinga <achinga.chris@gmail.com>
2023-04-14 01:30:32 +0300

    admin

--- polls/urls.py
+++ polls/urls.py
@@ -8,12 +8,7 @@ app_name = "polls"
 urlpatterns = [
     # ex: /polls/
     path("", views.index, name="index"),
-    # ex: /polls/5/
-    path("<int:question_id>/", views.detail, name="detail"),
-    # ex: /polls/5/results/
-    path("<int:question_id>/results/", views.results, name="results"),
-    # ex: /polls/5/vote/
-    path("<int:question_id>/vote/", views.vote, name="vote"),
+    path("vote/", views.voteDetails, name="votedetails"),
     path("nomination_category/<int:nomination_category_id>/", views.nomination_category_detail, name="nomination_category_detail"),
     path("category/<int:category_id>/vote/", views.nominee_vote, name="nominee_vote"),
     path('category/<int:category_id>/results/', views.nomination_category_results, name='nomination_category_results'),
--- polls/views.py
+++ polls/views.py
@@ -19,6 +19,12 @@ def index(request):
     context = {"latest_question_list": latest_question_list, "category": category}
     return render(request, "polls/index.html", context)
 
+def voteDetails(request):
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    category = NominationCategory.objects.all()
+    context = {"latest_question_list": latest_question_list, "category": category}
+    return render(request, "polls/votedetails.html", context)
+
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, "polls/detail.html", {"question": question})
