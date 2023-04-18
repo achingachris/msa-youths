fe8d8df259febb76badc1b27ba68d66ecec26a57
chris_achinga <achinga.chris@gmail.com>
2023-04-19 01:07:52 +0300

    update

--- polls/static/script.js
+++ polls/static/script.js
@@ -1,25 +1,3 @@
 console.log("Hello from script.js");
 
-function formatTime(seconds) {
-    const hours = Math.floor(seconds / 3600);
-    const minutes = Math.floor((seconds % 3600) / 60);
-    const sec = seconds % 60;
-    return `${hours}h ${minutes}m ${sec}s`;
-  }
-  
-  function updateCountdown(remainingTime, timeRemainingElement) {
-    if (remainingTime <= 0) {
-      clearInterval(interval);
-      timeRemainingElement.innerHTML = "You can now vote again!";
-    } else {
-      timeRemainingElement.innerHTML = formatTime(remainingTime);
-      remainingTime--;
-    }
-  }
-  
-  function startCountdown(remainingTime) {
-    const timeRemainingElement = document.getElementById("time-remaining");
-    const interval = setInterval(() => updateCountdown(remainingTime, timeRemainingElement), 1000);
-    updateCountdown(remainingTime, timeRemainingElement);
-  }
   
