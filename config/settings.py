9736d1f0a63912ab9598f45e173ebe2f010f290f
chris_achinga <achinga.chris@gmail.com>
2023-04-21 03:29:19 +0300

    fix

--- config/settings.py
+++ config/settings.py
@@ -14,7 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['chrisachinga.pythonanywhere.com', 'localhost', '127.0.0.1:8000']
 # ALLOWED_HOSTS = ['*']
